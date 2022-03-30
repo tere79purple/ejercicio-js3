26e8fc216b2ea21cf1210cc78171584e21128eac
Teresa Miguel <98185118+tere79purple@users.noreply.github.com>
2022-03-30 11:35:33 -0600

    Initial commit

---
+++ animales.js
@@ -0,0 +1,36 @@
+//Muestra un animal
+
+/* const doSomething=()=>{
+   if (document.getElementById(this.guau.index))
+   {
+    console.log("perro");
+   }else if(document.getElementById(miau)){
+    console.log("gato");
+   } else if(document.getElementById(bugs)){
+    console.log("conejo")
+   } else {
+    console.log("No has seleccionado")
+   }
+   
+}; */
+
+const damePerro=()=>{
+    console.log("     |\\_\/|                  \r\n     | @ @   Woof! \r\n     |   <>              _  \r\n     |  _\/\\------____ ((| |))\r\n     |               `--\' |   \r\n ____|_       ___|   |___.\' \r\n\/_\/_____\/____\/_______|")
+};
+
+const dameGato=()=>{
+    console.log(" ,_     _\r\n |\\\\_,-~\/\r\n \/ _  _ |    ,--.\r\n(  @  @ )   \/ ,-\'\r\n \\  _T_\/-._( (\r\n \/         `. \\\r\n|         _  \\ |\r\n \\ \\ ,  \/      |\r\n  || |-_\\__   \/\r\n ((_\/`(____,-\'");
+};
+
+const dameConejo=()=>{
+    console.log("                      \/|      __\r\n*             +      \/ |   ,-~ \/             +\r\n     .              Y :|  \/\/  \/                .         *\r\n         .          | jj \/( .^     *\r\n               *    >-\"~\"-v\"              .        *        .\r\n*                  \/       Y\r\n   .     .        jo  o    |     .            +\r\n                 ( ~T~     j                     +     .\r\n      +           >._-\' _.\/         +\r\n               \/| ;-\"~ _  l\r\n  .           \/ l\/ ,-\"~    \\     +\r\n              \\\/\/\\\/      .- \\\r\n       +       Y        \/    Y\r\n               l       I     !\r\n               ]\\      _\\    \/\"\\\r\n              (\" ~----( ~   Y.  )\r\n          ~~~~~~~~~~~~~~~~~~~~~~~~~~");
+};
+
+const dameMono=()=>{
+   console.log("             .-\"\"\"-.\r\n             _\/-=-.   \\\r\n            (_|a a\/   |_\r\n             \/ \"  \\   ,_)\r\n        _    \\`=\' \/__\/\r\n       \/ \\_  .;--\'  `-.\r\n       \\___)\/\/      ,  \\\r\n        \\ \\\/;        \\  \\\r\n         \\_.|         | |\r\n          .-\\ \'     _\/_\/\r\n        .\'  _;.    (_  \\\r\n       \/  .\'   `\\   \\\\_\/\r\n      |_ \/       |  |\\\\\r\n     \/  _)       \/  \/ ||\r\n    \/  \/       _\/  \/  \/\/\r\n    \\_\/       ( `-\/  ||\r\n              \/  \/   \\\\ .-.\r\n              \\_\/     \\\'-\'\/")
+};
+
+
+const Salir =() =>{
+    console.clear();
+}
